eb55ef8f07649b5489b177ab9cf105eb1e23d33a
mtrubey <mtrubey@angelsit.ru>
2022-02-02 22:21:31 +0300

    feat: add marvel project

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="navbar navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand">Марвел</a>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Поиск" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Поиск</button>
+      </form>
+    </div>
+  </nav>
+</template>
+
+<script>
+    export default {
+        name: "AppHeader",
+        data() {
+            return {
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
